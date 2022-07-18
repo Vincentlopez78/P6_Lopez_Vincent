41c6388a2a59f2bf1da9a37c3488b7cc8cd6db0d
Vincentlopez78 <99344889+Vincentlopez78@users.noreply.github.com>
2022-07-18 16:09:47 +0200

    maj nodemon

--- backend/middleware/limit.js
+++ backend/middleware/limit.js
@@ -1,10 +1,7 @@
+// @ts-nocheck
 const rateLimit = require('express-rate-limit');
 
-// @ts-ignore
-const connexionLimiter = rateLimit({
-    windowMs: 10 * 60 * 1000,
-    max: 5,
-    message: 'Vous avez essayer de vous connecter trop de fois, veuillez reessayer plus tard.'
+exports.connexionLimiter = rateLimit({
+        windowMs: 10 * 60 * 1000,
+        max: 5
 });
-
-module.exports = connexionLimiter;
