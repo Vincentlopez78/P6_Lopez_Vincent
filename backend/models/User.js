323d91eb80a3cb64472532f0a3e80f4d6968a4ca
Vincentlopez78 <99344889+Vincentlopez78@users.noreply.github.com>
2022-07-19 11:02:00 +0200

    ajout du token

--- backend/models/User.js
+++ backend/models/User.js
@@ -5,8 +5,7 @@ const userSchema =  new mongoose.Schema({
     email: {type: String, required: true, unique: true},
     password: {type: String, required: true}
 });
-//Plugin qui ajoute une validation de pré-enregistrement pour les champs uniques dans un schéma Mongoose.
-//Remplace une erreur E11000 de MongoDB par une erreur de validation.
+
 userSchema.plugin(uniqueValidator);
 
 module.exports = mongoose.model('User', userSchema);
